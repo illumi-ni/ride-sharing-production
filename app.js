e7509f45346063c19b302d6ff49eaa1e52b070c2
illumi-ni <krazyme53@gmail.com>
2021-07-23 08:58:12 +0545

    deployment

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, './client/build' ));
-    })
-}
+    });
+};
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
