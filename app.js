a054a35dbe015d98338c4f9966363c078248b017
illumi-ni <krazyme53@gmail.com>
2021-07-23 08:11:10 +0545

    deployment

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.use(admin_route);
 app.use(booking_route);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
+    app.use(express.static( './client/build' ));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build' ));
+        res.sendFile(path.join(__dirname, './client/build' ));
     })
 }
 
-const PORT = process.env.PORT || 90;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
